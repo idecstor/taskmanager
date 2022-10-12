bb12f266d8520f9f70765ce58e747b2af13fe86c
idecstor <egor.zakusilo.96@yandex.by>
2022-10-12 14:29:01 +0300

    added container for header in base.html added new class in models.py created superuser

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import sys
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField('Task name', max_length=50)
+    task = models.TextField('Description')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+
