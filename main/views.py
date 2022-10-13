01db58cf619fae600d2facded06ee0fd89483e1c
idecstor <egor.zakusilo.96@yandex.by>
2022-10-13 23:38:43 +0300

    added forms.py for page create.html need to fix

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Task
-
+from .forms import TaskForm
 
 def index(request):
     tasks = Task.objects.order_by('-id')
@@ -10,4 +10,8 @@ def about(request):
     return render(request, 'main/about.html')
 
 def create(request):
-    return render(request, 'create/.html')
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/create.html')
